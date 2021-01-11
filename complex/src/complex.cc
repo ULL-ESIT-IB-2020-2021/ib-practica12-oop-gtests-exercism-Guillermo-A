b0b1861336aacbb5f5f3990cb2691a17358904a6
Guillermo-A <alu0101432977@ull.edu.es>
2021-01-11 16:52:15 +0000

    programa mejorado visualmente un poco

--- complex/src/complex.cc
+++ complex/src/complex.cc
@@ -12,17 +12,23 @@ int main(int argc, char*argv[]){
     real2 = atof(argv[3]);
     imag2 = atof(argv[4]);
     Complex z1(real1, imag1);
+    std::cout << "Primer Complejo(z1): " << std::endl;
     z1.print_complex();
     Complex z2(real2, imag2);
+    std::cout << "Segundo Complejo(z2): " << std::endl;
     z2.print_complex();
     Complex result;
     result = z1.add(z2);
+    std::cout << "Suma entre z1 y z2" << std::endl;
     result.print_complex();
     result = z1.sub(z2);
+    std::cout << "Resta entre z1 y z2" << std::endl;
     result.print_complex();
     result = z1.mult(z2);
+    std::cout << "Multiplicacion entre z1 y z2" << std::endl;
     result.print_complex();
     result = z1.div(z2);
+    std::cout << "Division entre z1 y z2" << std::endl;
     result.print_complex();
     return 0;
 }
--- complex/test/test_sub.cc
+++ complex/test/test_sub.cc
@@ -1,6 +1,3 @@
 #include <gtest/gtest.h>
 #include "complex.h"
 
-TEST(IntegerFunctionTest, sub) {
-        EXPECT_DOUBLE_EQ((2.0), sum(1.0,0,1.0,0));
-}
